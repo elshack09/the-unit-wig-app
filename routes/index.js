62a9ca2bd8eea09212bcc01c20b5248fd36f3259
elshack09 <elshack09@gmail.com>
2018-06-09 21:37:41 -0400

    added bootstrap

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'The Unit' });
+});
+// render function tells node.js or express to please send back a response to the browser containting the HTML code of this index file but before sending it back replace all hoooks 
+module.exports = router;
