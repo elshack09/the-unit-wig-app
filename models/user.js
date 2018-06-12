d7a871697eaf3dc14e557779da07051a58382fff
elshack09 <elshack09@gmail.com>
2018-06-12 12:26:01 -0400

    added schema folder and switched up model file

--- models/user.js
+++ models/user.js
@@ -1,10 +1,5 @@
-
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const userSchema = require('../db/schema/user')
+const User = mongoose.model('User')
 
-const userSchema = new Schema({
-    name: String,
-    email: {type: String, required: true},
-    password: {type:String, required: true}
-});
-module.exports= mongoose.model('User', userSchema)
+module.exports= User
--- models/wig.js
+++ models/wig.js
@@ -1,12 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const wigSchema = require('../db/schema/wig')
+const Wig = mongoose.model('Wig')
 
-const wigSchema = new Schema({
-    title: {type: String, required: true},
-    texture: {type:String, required: true},
-    description: {type:String, required: true},
-    length: {type:String, required: true},
-    cap: {type:String, required: true},
-    price: {type: Number, required: true}
-})
-module.exports= mongoose.model('Wig', wigSchema)
+module.exports= Wig
